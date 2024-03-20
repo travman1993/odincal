f0df9a0fe8d9a83a08abdb0a065cf235ae59b8b0
Travis Rodriguez <travisgolembiewski@gmail.com>
2024-03-19 20:19:26 -0400

    added starter js

---
+++ main.js
@@ -0,0 +1,17 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+document.addEventListener("DOMContentLoaded", function(){
+    let clear = document.querySelector("#clear-btn")
+    let equal = document.querySelector("#equal-btn")
+    let decimal = document.querySelector("#decimal-btn")
+
+    let numbers = document.querySelector("#number-btn")
+    let operator = document.querySelector("#operator-btn")
+
+    let previous = document.querySelector(".previous")
+    let current = document.querySelector(".current")
+
+    
+})
