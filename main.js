83cdd298b0f6ac486bff633a3722f9b062467fe2
Travis Rodriguez <travisgolembiewski@gmail.com>
2024-03-24 18:36:02 -0400

    js, errors everywhere , equal doesnt work yet....idkkkkkk, gonna keep working

--- main.js
+++ main.js
@@ -7,11 +7,74 @@ document.addEventListener("DOMContentLoaded", function(){
     let equal = document.querySelector("#equal-btn")
     let decimal = document.querySelector("#decimal-btn")
 
-    let numbers = document.querySelector("#number-btn")
-    let operator = document.querySelector("#operator-btn")
+    let numbers = document.querySelectorAll("#number-btn")
+    let operators = document.querySelectorAll("#operator-btn")
 
-    let previous = document.querySelector(".previous")
-    let current = document.querySelector(".current")
+    let previousScreen = document.querySelector(".previous")
+    let currentScreen = document.querySelector(".current")
 
-    
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener("click", function(){
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+
+    equal.addEventListener("click", function(){
+        calculate();
+        previousScreen.textContent = '';
+        currentScreen.textContent = previousValue;
+    })
+
+    decimal.addEventListener("click", function(){
+        addDecimal();
+    })
 })
+
+function handleNumber(num){
+    if(currentValue.length <= 5){
+    currentValue += num;
+    }
+}
+
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    } else if(opterator === "-"){ 
+        previousValue -= currentValue;
+    } else if(operator === "x"){
+        previousValue *= currentValue;
+    } else{
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = currentValue.toString();
+}
+
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += '.';
+    }
+}
